e86a599579dceec0af05c7374d5a2da813c099e9
FantasyerOuO <75434717+FantasyerOuO@users.noreply.github.com>
2022-04-25 22:46:57 +0800

    Create T5.py

---
+++ T5.py
@@ -0,0 +1,17 @@
+import imp
+
+
+import tkinter as tk
+import random as rd
+import pyperclip as pc
+win=tk.Tk()
+
+win.geometry('400x400+500+500') # '視窗x視窗y+座標x+座標y'
+win.config(bg='#323232')
+win.title('教學5實例')
+lb0=tk.Label(text='隨機座標生成器')
+
+
+
+
+win.mainloop()
